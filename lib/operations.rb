b7828c87391c514997f7ae38adcc2637fdf67db2
Terrell Murrell <littlerell12@gmail.com>
2020-09-11 20:15:41 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,5 @@
+require pry
+
 def unsafe?(speed)
 	if speed > 60
 		return true
@@ -13,4 +15,5 @@ end
 def not_safe?(speed)
 	speed > 60 ? true : false
 	speed < 40 ? true : false
+	binding.pry
 end
